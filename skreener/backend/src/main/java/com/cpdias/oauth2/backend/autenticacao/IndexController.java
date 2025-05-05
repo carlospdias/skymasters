e63a43b508a6f6644503ec483e3df64e076850b2
Carlos Pereira Dias <carlospdiase@gmail.com>
2025-05-04 21:45:01 -0300

    :lock: Configura Securança usando Google Como provedor

---
+++ skreener/backend/src/main/java/com/cpdias/oauth2/backend/autenticacao/IndexController.java
@@ -0,0 +1,18 @@
+package com.cpdias.oauth2.backend.autenticacao;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(Model model, @AuthenticationPrincipal OAuth2User user) {
+        model.addAttribute("userName", user.getAttribute("name"));
+
+        return "autenticacao/index";
+    }
+}
